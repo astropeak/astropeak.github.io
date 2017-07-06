0438aba5b793e061874479c813a88785c9ccce13
Astropeak <astropeak@gmail.com>
2017-07-07 00:28:21 +0800

    aa

---
+++ config.el
@@ -0,0 +1,6 @@
+(setq op/repository-directory "/Users/astropeak/Dropbox/project/astropeak.github.io")
+
+(setq op/site-domain "http://astropeak.github.io/")
+(setq  op/site-main-title "自然语言处理")
+(setq  op/site-sub-title "")
+(setq  op/personal-github-link "https://github.com/astropeak")
