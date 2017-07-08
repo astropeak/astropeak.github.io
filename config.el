69bc4ea47367d0832b68189086474bd0472e7b86
Astropeak <astropeak@gmail.com>
2017-07-08 09:26:04 +0800

    Changes

--- config.el
+++ config.el
@@ -6,3 +6,24 @@
 (setq  op/personal-github-link "https://github.com/astropeak")
 
 
+(defun op/my-do-publication-and-preview-site (path)
+  "Do publication in PATH and preview the site in browser with simple-httpd.
+When invoked without prefix argument then PATH defaults to
+`op/site-preview-directory'."
+  (interactive
+   (if current-prefix-arg
+       (list (read-directory-name "Path: "))
+     (list op/site-preview-directory)))
+
+  ;; commit all changes
+  (op/git-change-branch op/repository-directory "source")
+  (op/git-commit-changes op/repository-directory "Changes")
+
+  (op/do-publication t nil path)
+
+  ;;change index to blog index
+  (copy-file (concat path "/blog/index.html") (concat path "/index.html") t t)
+
+  (httpd-serve-directory path)
+  (browse-url (format "http://%s:%d" system-name httpd-port)))
+
